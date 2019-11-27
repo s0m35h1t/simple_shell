dc942316354ac8b34de5728588222cb5ab9e8905
somairakia <somai.rakia@hotmail.fr>
2019-11-27 12:47:53 +0000

    setenv

--- environment.c
+++ environment.c
@@ -18,3 +18,36 @@ i++;
 }
 return (NULL);
 }
+/**
+* _setenv - get  variable
+* @name: variable name
+* @value: valiable value
+* @overwrite: overwited or no
+* Return: 0 or 1
+*/
+int _setenv(char *name, char *value, int overwrite)
+{
+int i = 0, j = 0;
+char *token;
+
+token = _strdup(name);
+token = str_concat(token, "=");
+token = str_concat(token, value);
+
+while (environ[i])
+{
+j = 0;
+while (name[j] == environ[i][j])
+j++;
+if (!name[j])
+{
+if (overwrite)
+{
+environ[i] = token;
+return (1);
+}
+}
+i++;
+}
+return (0);
+}
