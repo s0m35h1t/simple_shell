eec1261317d0db75f9ca1739ea4aa0130a21d287
doubix <adib.grouz@gmail.com>
2019-11-27 13:31:31 +0100

    funcs declarations

--- shell.h
+++ shell.h
@@ -18,4 +18,22 @@
 #define TOK_BUFSIZE 128
 #define TOK_DELIM " \t\r\n\a"
 #define ALS_DELIM "=''\t\r\n\a"
+
+/******Function Declarations******/
+
+char *_strdup(char *strtodup);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *strc1, char *strc2);
+unsigned int _strlen(char *str);
+char *_strtok(char *str, const char *delim);
+int _puts(const char *s);
+char *str_concat(char *s1, char *s2);
+int _putchar(char c);
+
+char **_realloc(char **ptr, size_t *size);
+
+char *_getenv(char *name);
+int _setenv(char *name, char *value, int overwrite);
+int _unsetenv(char *name);
+
 #endif /* _SHELL_H_ */
