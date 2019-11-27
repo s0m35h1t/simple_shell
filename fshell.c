bb5dfd52ea765bcc6dbb25e266c4fdfc193adbd4
doubix <adib.grouz@gmail.com>
2019-11-27 13:54:46 +0100

    builtins

--- fshell.c
+++ fshell.c
@@ -18,3 +18,24 @@ char *builtin_str[] = {
 "history",
 "setenv",
 "unsetenv"};
+
+int (*builtin_func[])(char **) = {
+&cd,
+&help,
+&exitshell,
+&alias,
+&env,
+&history,
+&b_setenv,
+&b_unsetenv};
+
+char *builtins_help[] = {
+"Change the shell working directory.\nUSAGE : cd PATH\n",
+"USAGE: help bultin\n   Display information about builtin commands\n",
+"exit: exit [n]\n   Exit the shell. ",
+"alias: alias [name[=value] ... ]\n   Define or display aliases.\n",
+"env: prints the current environment\n",
+"history : Display the history list",
+"USAGE: setenv NAME VALUE\n   Initialize a new environment variable, or modify an existing one\n",
+"USAGE: unsetenv NAME\n   Remove an environment variable"};
+
